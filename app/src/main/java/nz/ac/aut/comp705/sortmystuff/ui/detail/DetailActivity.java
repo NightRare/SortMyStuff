fa973c2e30a762efc9f9ebd46f612b8f70a5aa17
donnacello <donnadychinco@gmail.com>
2017-05-07 08:27:21 +1200

    Delete unnecessary imports

--- app/src/main/java/nz/ac/aut/comp705/sortmystuff/ui/detail/DetailActivity.java
+++ app/src/main/java/nz/ac/aut/comp705/sortmystuff/ui/detail/DetailActivity.java
@@ -2,14 +2,11 @@ package nz.ac.aut.comp705.sortmystuff.ui.detail;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
