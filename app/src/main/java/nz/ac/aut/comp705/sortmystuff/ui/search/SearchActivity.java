34bc63b0c0da34c7514723a939f8539a48a93d92
donnacello <donnadychinco@gmail.com>
2017-05-28 17:41:25 +1200

    Implement Search Assets user story

---
+++ app/src/main/java/nz/ac/aut/comp705/sortmystuff/ui/search/SearchActivity.java
@@ -0,0 +1,15 @@
+package nz.ac.aut.comp705.sortmystuff.ui.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nz.ac.aut.comp705.sortmystuff.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
